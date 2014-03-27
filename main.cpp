73e1cc563d15f744595056aef0e04028ba036152
felicia-christie <waxrain1217@gmail.com>
2014-03-28 01:44:43 +0700

    ok

--- main.cpp
+++ main.cpp
@@ -1,15 +1,11 @@
 #include "Bank.h"
 #include "DateTime.h"
-<<<<<<< HEAD
 #include "Event.h"
-=======
->>>>>>> 07b42b6f6ca4bdbb31456b10eafe6f5681ace60b
 
 using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
     int jumlahTeller;
     cin >> jumlahTeller; //baca masukan jumlah teller
     Bank BENI(jumlahTeller);
@@ -25,29 +21,6 @@ int main()
             BENI.Departure();
         Event2.BacaEvent(); //test baca masukan
 	}
-=======
-    Bank BENI(4);
-     BENI.Arrival();
-    cout << "setelah arr" << endl;
-    BENI.Arrival();
-    cout << "setelah arr" << endl;
-    BENI.Departure(1);
-    cout << "setelah dep" << endl;
-    BENI.Arrival();
-    cout << "setelah arr" << endl;
-    BENI.Departure(2);
-    cout << "setelah dep" << endl;
 
-   /* BENI.Arrival();
-    cout << "setelah arr" << endl;
-
-
-    BENI.Arrival();
-    cout << "setelah arr" << endl;
-    BENI.Arrival();
-    cout << "setelah arrii" << endl;
-    BENI.Departure(3);
-    cout << "SELESAI Arr!"; */
->>>>>>> 07b42b6f6ca4bdbb31456b10eafe6f5681ace60b
     return 0;
 }
